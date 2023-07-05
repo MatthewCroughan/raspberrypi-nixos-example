eed0e68265ba26d5e10b117790817ba2d17e14c8
matthewcroughan <matt@croughan.sh>
2023-07-05 13:06:18 +0100

    base: add some more necessary configuration

--- base.nix
+++ base.nix
@@ -9,6 +9,15 @@
     { device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
     };
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
-  nix.settings.experimental-features = lib.mkDefault "nix-command flakes";
+  boot = {
+    kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+    loader = {
+      generic-extlinux-compatible.enable = lib.mkDefault true;
+      grub.enable = lib.mkDefault false;
+    };
+  };
+  nix.settings = {
+    experimental-features = lib.mkDefault "nix-command flakes";
+    trusted-users = [ "root" "@wheel" ];
+  };
 }
