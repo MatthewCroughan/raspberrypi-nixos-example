1a9f5fd2f2032eeaa58f272311677d1962110302
matthewcroughan <matt@croughan.sh>
2023-12-14 15:15:30 +0000

    update

--- flake.nix
+++ flake.nix
@@ -3,17 +3,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:nixos/nixos-hardware";
   };
-  outputs = { self, nixpkgs, nixos-hardware }: {
+  outputs = { self, nixpkgs, nixos-hardware }: rec {
     images = {
       pi = (self.nixosConfigurations.pi.extendModules {
-        modules = [ "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-new-kernel-no-zfs-installer.nix" ];
+        modules = [
+          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+          {
+            disabledModules = [ "profiles/base.nix" ];
+          }
+        ];
       }).config.system.build.sdImage;
     };
+    packages.x86_64-linux.pi-image = images.pi;
+    packages.aarch64-linux.pi-image = images.pi;
     nixosConfigurations = {
       pi = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         modules = [
           nixos-hardware.nixosModules.raspberry-pi-4
+          "${nixpkgs}/nixos/modules/profiles/minimal.nix"
           ./configuration.nix
           ./base.nix
         ];
