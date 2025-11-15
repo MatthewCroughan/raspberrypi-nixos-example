85b24f16302d01131a56d7eca0224eca91dfb284
Brian McGee <brian@bmcgee.ie>
2025-11-15 11:02:05 +0000

    feat: add pi zero 2 w support

--- flake.nix
+++ flake.nix
@@ -15,6 +15,12 @@
           echo -e "M\nt\n1\n0b\nw\nr\nw\n" | ${pkgs.util-linux}/bin/fdisk ${self.nixosConfigurations.pi3.config.image.repart.imageFileBasename}.raw
         '';
       };
+      pi-zero-2-w = self.nixosConfigurations.pi-zero-2-w.config.system.build.image.overrideAttrs {
+        preInstall = ''
+          ${pkgs.gptfdisk}/bin/sgdisk --hybrid 1:EE ${self.nixosConfigurations.pi-zero-2-w.config.image.repart.imageFileBasename}.raw
+          echo -e "M\nt\n1\n0b\nw\nr\nw\n" | ${pkgs.util-linux}/bin/fdisk ${self.nixosConfigurations.pi-zero-2-w.config.image.repart.imageFileBasename}.raw
+        '';
+      };
       pi0 = self.nixosConfigurations.pi0.config.system.build.image.overrideAttrs {
         preInstall = ''
           ${pkgs.gptfdisk}/bin/sgdisk --hybrid 1:EE ${self.nixosConfigurations.pi0.config.image.repart.imageFileBasename}.raw
@@ -26,6 +32,8 @@
     packages.aarch64-linux.pi-image = images.pi4;
     packages.x86_64-linux.pi3-image = images.pi3;
     packages.aarch64-linux.pi3-image = images.pi3;
+    packages.x86_64-linux.pi-zero-2-w-image = images.pi-zero-2-w;
+    packages.aarch64-linux.pi-zero-2-w-image = images.pi-zero-2-w;
     packages.x86_64-linux.pi0-image = images.pi0;
     packages.aarch64-linux.pi0-image = images.pi0;
     nixosConfigurations = {
@@ -42,6 +50,15 @@
           }
           "${nixpkgs}/nixos/modules/profiles/minimal.nix"
           ./repart-pi0.nix
+          ./configuration.nix
+        ];
+      };
+      pi-zero-2-w = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          nixos-hardware.nixosModules.raspberry-pi-3
+          "${nixpkgs}/nixos/modules/profiles/minimal.nix"
+          ./repart-pi-zero-2-w.nix
           ./configuration.nix
         ];
       };
